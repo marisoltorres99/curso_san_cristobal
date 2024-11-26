33729f1a7fba03b2c5f1a94d727e8ee5d6e71ea0
marisoltorres99 <86800286+marisoltorres99@users.noreply.github.com>
2024-11-26 20:49:29 -0300

    agrego ejercicio 5

--- practica_front/angular_app/src/app/app.component.ts
+++ practica_front/angular_app/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Ejercicio2Component } from "./ejercicio2/ejercicio2.component";
 import { Ejercicio3Component } from "./ejercicio3/ejercicio3.component";
 import { Ejercicio4Component } from "./ejercicio4/ejercicio4.component";
+import { Ejercicio5Component } from './ejercicio5/ejercicio5.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Ejercicio2Component, Ejercicio3Component, Ejercicio4Component],
+  imports: [RouterOutlet, Ejercicio2Component, Ejercicio3Component, Ejercicio4Component, Ejercicio5Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
