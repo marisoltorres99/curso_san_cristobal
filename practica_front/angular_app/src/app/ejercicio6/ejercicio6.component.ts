82864f1fe924611aba6b63499b440e957080999e
marisoltorres99 <86800286+marisoltorres99@users.noreply.github.com>
2024-11-26 21:01:13 -0300

    agrego ejercicio 6

---
+++ practica_front/angular_app/src/app/ejercicio6/ejercicio6.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ejercicio6',
+  imports: [],
+  templateUrl: './ejercicio6.component.html',
+  styleUrl: './ejercicio6.component.css'
+})
+export class Ejercicio6Component {
+  texto_footer: string = "2024 Fullstack."
+}
