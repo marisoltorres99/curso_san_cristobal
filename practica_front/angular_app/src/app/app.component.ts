ba9a961a92a82195bfdce26a05690c89cc0168d7
marisoltorres99 <86800286+marisoltorres99@users.noreply.github.com>
2024-12-17 16:11:46 -0300

    borro componentes que ya no se usan en el archivo principal

--- practica_front/angular_app/src/app/app.component.ts
+++ practica_front/angular_app/src/app/app.component.ts
@@ -1,18 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Ejercicio2Component } from "./ejercicio2/ejercicio2.component";
-import { Ejercicio3Component } from "./ejercicio3/ejercicio3.component";
-import { Ejercicio4Component } from "./ejercicio4/ejercicio4.component";
 import { Ejercicio5Component } from './ejercicio5/ejercicio5.component';
 import { Ejercicio6Component } from './ejercicio6/ejercicio6.component';
-import { Ejercicio7SearchbarComponent } from "./ejercicio7-searchbar/ejercicio7-searchbar.component";
-import { MovieListComponent } from './movie-list/movie-list.component';
-import { MovieItemComponent } from './movie-item/movie-item.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Ejercicio2Component, Ejercicio3Component, Ejercicio4Component, 
-    Ejercicio5Component, Ejercicio6Component, Ejercicio7SearchbarComponent, MovieListComponent],
+  imports: [RouterOutlet, Ejercicio5Component, Ejercicio6Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
