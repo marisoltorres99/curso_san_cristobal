4ad1d651d10ef48b9bf5de41982201fc52955bce
marisoltorres99 <86800286+marisoltorres99@users.noreply.github.com>
2024-12-17 16:08:37 -0300

    creo rutas

--- practica_front/angular_app/src/app/app.routes.ts
+++ practica_front/angular_app/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { EjerciciosComponent } from './ejercicios/ejercicios.component';
+import { MoviesComponent } from './movies/movies.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'pelis',
+        component: MoviesComponent
+    },
+
+    {
+        path: 'ejercicios',
+        component: EjerciciosComponent
+    }
+];
